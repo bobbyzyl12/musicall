05870f4b83d51a022e54671672fa78848b270ae2
milkian <cyx.ller@gmail.com>
2016-04-25 00:36:53 -0700

    configure sae

--- musicall/musicall/settings.py
+++ musicall/musicall/settings.py
@@ -74,12 +74,7 @@ WSGI_APPLICATION = 'musicall.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
