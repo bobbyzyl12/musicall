18539b25b5e9bc3b906c9b33a611212a9e321efd
milkian <cyx.ller@gmail.com>
2016-04-25 00:28:14 -0700

    config sae

--- musicall/musicall/wsgi.py
+++ musicall/musicall/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musicall.settings")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "musicall.musicall.settings")
 
 application = get_wsgi_application()
